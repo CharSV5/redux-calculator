03ab52e5e5b8ea6c8b68e2d11e613ff0f1a3bab8
charlene bastians <charlenebastians@hotmail.com>
2018-09-03 09:45:55 +0100

    tests passing for new input

--- src/test/sum-action.test.js
+++ src/test/sum-action.test.js
@@ -4,12 +4,32 @@ const newInput1 = 3;
 const newInput2 = 4;
 
 describe('Sum actions', () => {
-    it('should add input1 and input2 together ', () => {
-        const sum = 7
-        const expectedAction = {
-            type: sumActions.ADD,
-            newAnswer: sum
-        }
-        expect(sumActions.add(newInput1, newInput2)).toEqual(expectedAction)
+    describe('add', () => {
+        it('should add input1 and input2 together ', () => {
+            const sum = 7
+            const expectedAction = {
+                type: sumActions.ADD,
+                newAnswer: sum
+            }
+            expect(sumActions.add(newInput1, newInput2)).toEqual(expectedAction)
+        })
+    })
+    describe('record_Input_1', ()=> {
+        it('should record the first input', () => {
+            const expectedAction = {
+                type: sumActions.RECORD_INPUT_1,
+                newInput1: newInput1
+            }
+            expect(sumActions.Record_Input_1(newInput1)).toEqual(expectedAction)
+        })
+    })
+    describe('record_Input_2', ()=> {
+        it('should record the second input', () => {
+            const expectedAction = {
+                type: sumActions.RECORD_INPUT_2,
+                newInput2: newInput2
+            }
+            expect(sumActions.Record_Input_2(newInput2)).toEqual(expectedAction)
+        })
     })
 })
